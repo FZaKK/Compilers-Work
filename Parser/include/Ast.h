01576e5db13eb988b18c32da8ecb9ee3594d4d10
FZaKK <2014074@mail.nankai.edu.cn>
2022-11-04 23:21:12 +0800

    next

--- Parser/include/Ast.h
+++ Parser/include/Ast.h
@@ -10,28 +10,33 @@ class Node
 private:
     static int counter;
     int seq;
+    Node* next;
 public:
     Node();
-    int getSeq() const {return seq;};
+    int getSeq() const { return seq; };
     virtual void output(int level) = 0;
+    void setNext(Node* node);
+    void setAdjNext(Node* node) { next = node; }
+    Node* getNext() { return next; }
+    void cleanNext() { next = nullptr; }
 };
 
 class ExprNode : public Node
 {
 protected:
-    SymbolEntry *symbolEntry;
+    SymbolEntry* symbolEntry;
 public:
-    ExprNode(SymbolEntry *symbolEntry) : symbolEntry(symbolEntry){};
+    ExprNode(SymbolEntry* symbolEntry) : symbolEntry(symbolEntry) {};
 };
 
 class UnaryExpr : public ExprNode
 {
 private:
     int op;
-    ExprNode *expr;
+    ExprNode* expr;
 public:
-    enum {ADD, SUB, NON};
-    UnaryExpr(SymbolEntry *se, int op, ExprNode*expr) : ExprNode(se), op(op), expr(expr){};    
+    enum { ADD, SUB, NON };
+    UnaryExpr(SymbolEntry* se, int op, ExprNode* expr) : ExprNode(se), op(op), expr(expr) {};
     void output(int level);
 };
 
@@ -39,24 +44,24 @@ class BinaryExpr : public ExprNode
 {
 private:
     int op;
-    ExprNode *expr1, *expr2;
+    ExprNode* expr1, * expr2;
 public:
-    enum {ADD, SUB, MUL, DIV, MOD, AND, OR, LESS, GREATER, GORE, LORE, EQUAL, NOTEQUAL};
-    BinaryExpr(SymbolEntry *se, int op, ExprNode*expr1, ExprNode*expr2) : ExprNode(se), op(op), expr1(expr1), expr2(expr2){};
+    enum { ADD, SUB, MUL, DIV, MOD, AND, OR, LESS, GREATER, GORE, LORE, EQUAL, NOTEQUAL };
+    BinaryExpr(SymbolEntry* se, int op, ExprNode* expr1, ExprNode* expr2) : ExprNode(se), op(op), expr1(expr1), expr2(expr2) {};
     void output(int level);
 };
 
 class Constant : public ExprNode
 {
 public:
-    Constant(SymbolEntry *se) : ExprNode(se){};
+    Constant(SymbolEntry* se) : ExprNode(se) {};
     void output(int level);
 };
 
 class Id : public ExprNode
 {
 public:
-    Id(SymbolEntry *se) : ExprNode(se){};
+    Id(SymbolEntry* se) : ExprNode(se) {};
     void output(int level);
 };
 
@@ -66,77 +71,77 @@ class StmtNode : public Node
 class CompoundStmt : public StmtNode
 {
 private:
-    StmtNode *stmt;
+    StmtNode* stmt;
 public:
-    CompoundStmt(StmtNode *stmt) : stmt(stmt) {};
+    CompoundStmt(StmtNode* stmt) : stmt(stmt) {};
     void output(int level);
 };
 
 class SeqNode : public StmtNode
 {
 private:
-    StmtNode *stmt1, *stmt2;
+    StmtNode* stmt1, * stmt2;
 public:
-    SeqNode(StmtNode *stmt1, StmtNode *stmt2) : stmt1(stmt1), stmt2(stmt2){};
+    SeqNode(StmtNode* stmt1, StmtNode* stmt2) : stmt1(stmt1), stmt2(stmt2) {};
     void output(int level);
 };
 
 class DeclStmt : public StmtNode
 {
 private:
-    Id *id;
+    Id* id;
 public:
-    DeclStmt(Id *id) : id(id){};
+    DeclStmt(Id* id) : id(id) {};
     void output(int level);
 };
 
 class IfStmt : public StmtNode
 {
 private:
-    ExprNode *cond;
-    StmtNode *thenStmt;
+    ExprNode* cond;
+    StmtNode* thenStmt;
 public:
-    IfStmt(ExprNode *cond, StmtNode *thenStmt) : cond(cond), thenStmt(thenStmt){};
+    IfStmt(ExprNode* cond, StmtNode* thenStmt) : cond(cond), thenStmt(thenStmt) {};
     void output(int level);
 };
 
 class IfElseStmt : public StmtNode
 {
 private:
-    ExprNode *cond;
-    StmtNode *thenStmt;
-    StmtNode *elseStmt;
+    ExprNode* cond;
+    StmtNode* thenStmt;
+    StmtNode* elseStmt;
 public:
-    IfElseStmt(ExprNode *cond, StmtNode *thenStmt, StmtNode *elseStmt) : cond(cond), thenStmt(thenStmt), elseStmt(elseStmt) {};
+    IfElseStmt(ExprNode* cond, StmtNode* thenStmt, StmtNode* elseStmt) : cond(cond), thenStmt(thenStmt), elseStmt(elseStmt) {};
     void output(int level);
 };
 
 class ReturnStmt : public StmtNode
 {
 private:
-    ExprNode *retValue;
+    ExprNode* retValue;
 public:
-    ReturnStmt(ExprNode*retValue) : retValue(retValue) {};
+    ReturnStmt(ExprNode* retValue) : retValue(retValue) {};
     void output(int level);
 };
 
 class AssignStmt : public StmtNode
 {
 private:
-    ExprNode *lval;
-    ExprNode *expr;
+    ExprNode* lval;
+    ExprNode* expr;
 public:
-    AssignStmt(ExprNode *lval, ExprNode *expr) : lval(lval), expr(expr) {};
+    AssignStmt(ExprNode* lval, ExprNode* expr) : lval(lval), expr(expr) {};
     void output(int level);
 };
 
 class FunctionDef : public StmtNode
 {
 private:
-    SymbolEntry *se;
-    StmtNode *stmt;
+    SymbolEntry* se;
+    StmtNode* stmt;
 public:
-    FunctionDef(SymbolEntry *se, StmtNode *stmt) : se(se), stmt(stmt){};
+    FunctionDef(SymbolEntry* se, StmtNode* stmt) : se(se), stmt(stmt) {};
     void output(int level);
 };
 
@@ -145,8 +150,8 @@ class Ast
 private:
     Node* root;
 public:
-    Ast() {root = nullptr;}
-    void setRoot(Node*n) {root = n;}
+    Ast() { root = nullptr; }
+    void setRoot(Node* n) { root = n; }
     void output();
 };
 
